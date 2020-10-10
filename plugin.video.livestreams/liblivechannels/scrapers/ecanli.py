e2364dc4ad39b6c42d1cb765e7170a8805b71c9b
hbiyik <huseyinbiyik@hotmail.com>
2020-10-10 22:44:20 +0200

    livestreams: enable ecanli

--- plugin.video.livestreams/liblivechannels/scrapers/ecanli.py
+++ plugin.video.livestreams/liblivechannels/scrapers/ecanli.py
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-"""
+
 from liblivechannels import scraper, scrapers
 import re
 import htmlement
@@ -140,4 +140,3 @@ def iterexternal(download, cid):
     if ecanli_channel:
         for media in ecanli_channel(download).get():
             yield media
-"""
