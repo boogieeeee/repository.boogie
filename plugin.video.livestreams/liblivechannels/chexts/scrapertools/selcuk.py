ad2051e8617035d59b28eeca752701a915f66309
hbiyik <huseyinbiyik@hotmail.com>
2021-02-14 04:02:32 +0100

    livestreams: add dmax

--- plugin.video.livestreams/liblivechannels/chexts/scrapertools/selcuk.py
+++ plugin.video.livestreams/liblivechannels/chexts/scrapertools/selcuk.py
@@ -52,12 +52,21 @@ def itermedias(chfilter):
             up = urlparse.urlparse(links[0])
             chdict = dict(urlparse.parse_qsl(up.query))
             if "id" in chdict:
-                chid = chdict.get("id")
-                kourl = "%s://%s/keslanorospucocugu.js" % (up.scheme, up.netloc)
-                subpath = re.search("dmz\+(?:\'|\")(.+?)(?:\'|\")", net.http(kourl, referer=cfg.selcuk)).group(1)
-                jsurl = "%s://%s/dmzjsn.json" % (up.scheme, up.netloc)
-                sdomain = json.loads(net.http(jsurl, referer=url, headers={"x-requested-with": "XMLHttpRequest"}))["d"]
-                media = "https://xxx.%s%s%s/strmrdr.m3u8" % (sdomain, subpath, chid)
+                subpage = net.http(links[0], referer=cfg.selcuk)
+                embeds = re.findall("window\[\\'atob\\']\(\"(.+?)\"\)", subpage)
+                media = None
+                if len(embeds) == 2 and len(embeds[1]) > 10:
+                    try:
+                        media = embeds[1].decode("base64")
+                    except Exception:
+                        pass
+                if not media:
+                    chid = chdict.get("id")
+                    kourl = "%s://%s/keslanorospucocugu.js" % (up.scheme, up.netloc)
+                    subpath = re.search("dmz\+(?:\'|\")(.+?)(?:\'|\")", net.http(kourl, referer=cfg.selcuk)).group(1)
+                    jsurl = "%s://%s/dmzjsn.json" % (up.scheme, up.netloc)
+                    sdomain = json.loads(net.http(jsurl, referer=url, headers={"x-requested-with": "XMLHttpRequest"}))["d"]
+                    media = "https://xxx.%s%s%s/strmrdr.m3u8" % (sdomain, subpath, chid)
             elif "stream" in chdict:
                 strid = chdict.get("stream")
                 up = urlparse.urlparse(links[1])
