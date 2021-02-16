d6a83ab90dfa7ec1118e59c7daa9dc17bdd231fa
hbiyik <huseyinbiyik@hotmail.com>
2021-02-17 00:34:39 +0100

    animeturk: new addon

---
+++ service.vods.animeturk/lib/animeturk.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+'''
+    Author    : Huseyin BIYIK <husenbiyik at hotmail>
+    Year      : 2016
+    License   : GPL
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import vods
+import htmlement
+import re
+import json
+import traceback
+import jscrypto
+
+from tinyxbmc import net
+from tinyxbmc import tools
+from tinyxbmc import gui
+
+
+domain = "https://www.turkanime.net/"
+ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+
+
+class animeturk(vods.showextension):
+    usedirect = False
+    useaddonplayers = False
+
+    info = {"title": u"Türk Anime TV"
+            }
+    ismovie = False
+
+    def getpage(self, *args, **kwargs):
+        kwargs["timeout"] = 30
+        kwargs["http2"] = True
+        return self.download(*args, **kwargs)
+
+    def getcategories(self):
+        xpage = htmlement.fromstring(self.getpage("%sajax/turler" % domain,
+                                                  headers={"x-requested-with": "XMLHttpRequest"},
+                                                  referer=domain))
+        for a in xpage.iterfind(".//a"):
+            self.additem(a.get("title"), net.absurl(a.get("href"), domain))
+
+    def getshows(self, catargs=None):
+        if catargs:
+            self.scrapegrid(htmlement.fromstring(self.getpage(catargs)))
+
+    def scrapegrid(self, xpage):
+        for div in xpage.iterfind(".//div[@class='panel panel-visible']"):
+            a = div.find(".//a[@class='baloon']")
+            img = net.absurl(div.find(".//img").get("data-src"), domain)
+            art = {"icon": img, "thumb": img, "poster": img}
+            title = a.get("title").replace("izle", "").strip()
+            url = net.absurl(a.get("href"), domain)
+            if "/anime/" in url:
+                imgid = re.search("([0-9]+)", img)
+                if not imgid:
+                    continue
+                url = imgid.group(1), art
+            self.additem(title, url, art=art)
+
+    def searchshows(self, keyword=None):
+        page = self.getpage(domain + "arama", data={"arama": keyword}, method="POST")
+        self.scrapegrid(htmlement.fromstring(page))
+
+    def getepisodes(self, showargs=None, seaargs=None):
+        if showargs:
+            aniid, art = showargs
+            url = "%sajax/bolumler&animeId=%s" % (domain, aniid)
+            page = self.getpage(url, headers={"x-requested-with": "XMLHttpRequest"})
+            for a in htmlement.fromstring(page).iterfind(".//a"):
+                href = a.get("href")
+                if href and "/video/" in href:
+                    title = a.get("title")
+                    url = net.absurl(a.get("href"), domain)
+                    self.additem(title, url, art=art)
+        else:
+            self.scrapegrid(htmlement.fromstring(self.getpage(domain)))
+
+    def getlink(self, mirrorlink, xmirrorpage=None):
+        try:
+            if not xmirrorpage:
+                mirrorpage = self.getpage(mirrorlink,
+                                          headers={"x-requested-with": "XMLHttpRequest"},
+                                          referer=domain)
+                xmirrorpage = htmlement.fromstring(mirrorpage)
+            iframe = net.absurl(xmirrorpage.find(".//iframe").get("src"), domain)
+            iframesrc = self.getpage(iframe,
+                                     referer=domain)
+            iframe2 = json.loads(re.search("var\s*?iframe\s*?\=\s*?(?:\'|\")(.+)(?:\'|\")", iframesrc).group(1))
+            password = re.search("var\s*?pass\s*?\=\s*?(?:\'|\")(.+)(?:\'|\")", iframesrc).group(1)
+            link = jscrypto.decrypt(iframe2["ct"].decode("base64"),
+                                    password,
+                                    iframe2["s"].decode("hex"))
+            link = json.loads(link)
+            return net.absurl(link, domain)
+        except Exception:
+            print traceback.format_exc()
+
+    def iterajaxlink(self, xpage, xpath=None):
+        xpath = xpath or ".//button"
+        for button in xpage.iterfind(xpath):
+            link = button.get("onclick")
+            if link:
+                ajaxlink = re.search("\((?:\"|\')(ajax.+?)(?:\"|\')", link)
+                if ajaxlink:
+                    tag = tools.elementsrc(button).encode("ascii", "replace").strip()
+                    yield tag, net.absurl(ajaxlink.group(1), domain)
+
+    def geturls(self, id):
+        fansubxpath = ".//div[@class='panel-body']/div[1]/button"
+        mirrorxpath = ".//div[@class='panel-body']/div[4]/button"
+
+        xpage = htmlement.fromstring(self.getpage(id, referer=domain))
+
+        fansubs = {}
+
+        for fansub, fansublink in tools.safeiter(self.iterajaxlink(xpage, fansubxpath)):
+            fansubs[fansub] = fansublink
+
+        if not fansubs:
+            for _, mirrorlink in tools.safeiter(self.iterajaxlink(xpage, mirrorxpath)):
+                mirror = self.getlink(mirrorlink)
+                if mirror:
+                    yield mirror
+        else:
+            fansubselect = gui.select("Select Fansub", fansubs.keys())
+            i = -1
+            for _fansub, fansublink in fansubs.iteritems():
+                i += 1
+                if fansubselect == -1 or fansubselect == i:
+                    xfansubpage = htmlement.fromstring(self.getpage(fansublink,
+                                                                    headers={"x-requested-with": "XMLHttpRequest"},
+                                                                    referer=id))
+                    mirror = self.getlink(None, xfansubpage)
+                    if mirror:
+                        yield mirror
+                    for _, mirrorlink in tools.safeiter(self.iterajaxlink(xfansubpage, mirrorxpath)):
+                        mirror = self.getlink(mirrorlink)
+                        if mirror:
+                            yield mirror
---
+++ service.vods.animeturk/lib/jscrypto.py
@@ -0,0 +1,39 @@
+'''
+Created on Feb 16, 2021
+
+@author: boogie
+'''
+import hashlib
+import pyaes
+
+
+def evpKDF(passwd, salt, key_size=8, iv_size=4, iterations=1, hash_algorithm="md5"):
+    target_key_size = key_size + iv_size
+    derived_bytes = ""
+    number_of_derived_words = 0
+    block = None
+    hasher = hashlib.new(hash_algorithm)
+    while number_of_derived_words < target_key_size:
+        if block is not None:
+            hasher.update(block)
+        hasher.update(passwd)
+        hasher.update(salt)
+        block = hasher.digest()
+        hasher = hashlib.new(hash_algorithm)
+        for _ in range(1, iterations):
+            hasher.update(block)
+            block = hasher.digest()
+            hasher = hashlib.new(hash_algorithm)
+        derived_bytes += block[0: min(len(block), (target_key_size - number_of_derived_words) * 4)]
+        number_of_derived_words += len(block) / 4
+    return derived_bytes[0: key_size * 4], derived_bytes[key_size * 4:]
+
+
+def decrypt(data, password, salt):
+    key, _ = evpKDF(password, salt, key_size=12)
+    iv = key[len(key) - 16:]
+    key = key[:len(key) - 16]
+    decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationCBC(key, iv))
+    decryptedData = decrypter.feed(data)
+    decryptedData += decrypter.feed()
+    return decryptedData
