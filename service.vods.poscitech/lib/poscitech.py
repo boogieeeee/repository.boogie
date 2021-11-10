76f83d02e01593d4a989d0be022dc152246d0816
boogiepop <boogiepop@gmx.com>
2021-11-10 23:40:54 +0100

    dadylive: improve

--- service.vods.poscitech/lib/poscitech.py
+++ service.vods.poscitech/lib/poscitech.py
@@ -20,8 +20,10 @@
 
 import vods
 import re
+import htmlement
 from tinyxbmc import net
 from tinyxbmc import const
+from tinyxbmc import tools
 
 
 # references: "https://poscitech.club/tv/ch1.php, https://daddylive.me/"
@@ -30,6 +32,7 @@ sdom = "https://www.eplayer.to/"
 surl = sdom + "poscitech.php?live=%s&vw=100vw&vh=100vh"
 referer = "https://poscitech.club/"
 rgx = "source\s*?\:\s*?(?:\"|\")(.+?)(?:\"|\")"
+ddom = "https://daddylive.me/"
 
 
 class poscitech(vods.movieextension):
@@ -38,13 +41,43 @@ class poscitech(vods.movieextension):
     uselinkplayers = False
     dropboxtoken = const.DB_TOKEN
 
-    info = {"title": "Poscitech"
+    info = {"title": "DaddyLive"
             }
+    art = {"icon": "https://i.imgur.com/8EL6mr3.png",
+           "thumb": "https://i.imgur.com/8EL6mr3.png",
+           "poster": "https://i.imgur.com/8EL6mr3.png"
+           }
+
+    def getcategories(self):
+        page = self.download(ddom)
+        for m in re.finditer("hr\>(.+?)\<(.+?)(?:<\/p|<br\s\/>)", page):
+            title = m.group(1)
+            txt = m.group(2)
+            channels = []
+            for m in re.finditer("\<a.+?\>(.+?)\<\/a\>", txt):
+                chnum = re.search("\(CH\-([0-9]+)\)", m.group(1))
+                if chnum:
+                    chnum = int(chnum.group(1))
+                    channels.append((m.group(1), chnum))
+            if channels:
+                self.additem(title, channels)
 
     def getmovies(self, cat=None):
-        for i in range(1, 130 + 1):
-            pass
-            self.additem("Channel %s" % i, i)
+        if cat:
+            for ctxt, cnum in cat:
+                self.additem(ctxt, cnum)
+        else:
+            page = self.download(ddom + "/24-hours-channels.php")
+            chnames = {}
+            for a in htmlement.fromstring(page).iterfind(".//table/.//a"):
+                href = a.get("href")
+                if href is not None:
+                    chnum = re.search("stream\-([0-9]+)\.", href)
+                    if chnum:
+                        chnames[int(chnum.group(1))] = tools.elementsrc(a)
+            for i in range(1, 150 + 1):
+                chname = chnames.get(i, "Channel")
+                self.additem("%s (#%s)" % (chname, i), i)
 
     def geturls(self, streamid):
         url = surl % streamid
