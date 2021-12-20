dd4aae886769c83b126d2834b8033f7c5a3cac69
boogiepop <boogiepop@gmx.com>
2021-12-20 01:19:18 +0100

    livestreams: ssport2 epg

--- plugin.video.livestreams/liblivechannels/chexts/ssport.py
+++ plugin.video.livestreams/liblivechannels/chexts/ssport.py
@@ -10,7 +10,6 @@ try:
     class testssport2(ChannelTest, unittest.TestCase):
         index = "ssport:ssport2:"
         minlinks = 1
-        minepg = 0
 
 except ImportError:
     pass
@@ -18,6 +17,13 @@ except ImportError:
 
 from liblivechannels import scraper
 from liblivechannels.chexts.scrapertools.multi import multi
+from liblivechannels.chexts.scrapertools.mynetyayin import trmonmap, utctz, trtz, now
+from liblivechannels import programme
+import re
+import datetime
+
+from tinyxbmc import net
+import htmlement
 
 
 class ssport1(multi, scraper):
@@ -39,3 +45,31 @@ class ssport2(multi, scraper):
     ses_ids = ["ssport2-live"]
     selcuk_adaptive = False
     ses_adaptive = False
+
+    def iterprogrammes(self):
+        for prog in iterprogrammes():
+            yield prog
+
+
+def iterprogrammes():
+    u = "https://www.ssport.tv/yayin-akisi"
+    pagex = htmlement.fromstring(net.http(u))
+    prename = predate = None
+    for day in pagex.iterfind('.//ul[@id="switcher-day-s-sport-2"]/li'):
+        datadate = day.get("data-date")
+        if datadate is not None:
+            curmatch = re.search("([0-9]+)\s(.+?)\s", datadate)
+            curd = int(curmatch.group(1))
+            curm = trmonmap[curmatch.group(2).lower().strip()]
+            for prog in day.iterfind("./ul/li"):
+                pdate = prog.find(".//time")
+                pname = prog.find(".//h3")
+                if pdate is not None and pname is not None:
+                    phour, pmin = pdate.get("datetime").split(":")
+                    pdate = datetime.datetime(day=curd, month=curm, year=now.year,
+                                              hour=int(phour), minute=int(pmin), tzinfo=trtz)
+                    pname = pname.text.strip()
+                    if prename:
+                        yield programme(prename, predate, pdate)
+                    prename = pname
+                    predate = pdate
