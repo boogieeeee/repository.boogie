5a9c561c24700ba91faf5409fd408a8a126b0522
boogiepop <boogiepop@gmx.com>
2022-01-30 15:30:26 +0100

    livestreams: fix canlitv

--- plugin.video.livestreams/liblivechannels/chexts/scrapertools/canlitvcenter.py
+++ plugin.video.livestreams/liblivechannels/chexts/scrapertools/canlitvcenter.py
@@ -13,28 +13,32 @@ dom = "canlitv.center"
 domain = "https://" + dom
 
 rgxkey = "verianahtar[\s\t]*?\=[\s\t]*?(?:\"|\')(.+?)(?:\"|\')"
-rgxlink = "yayincomtr[0-9]+[\s\t]*?\=[\s\t]*?(?:\"|\')(.*?)(?:\"|\')"
+rgxlink = "yayin[a-zA-Z0-9]+[\s\t]*?\=[\s\t]*?(?:\"|\')(.*?)(?:\"|\')"
 
 
 def itermedias(ctvcid, ctvcids=None):
     if not ctvcids:
         ctvcids = [ctvcid]
     for ctvcid in ctvcids:
+        links = {}
         u = domain + "/" + ctvcid
         iframe1 = htmlement.fromstring(net.http(u, referer=domain)).find(".//iframe").get("src")
         iframe2 = htmlement.fromstring(net.http(iframe1, referer=u)).find(".//iframe").get("src")
         src = net.http(iframe2, referer=iframe1)
         media = re.search("file[\s\t]*?\:[\s\t]*?atob\((?:\"|\')(.+?)(?:\"|\')\)", src)
         if media:
-            yield net.hlsurl(base64.b64decode(media.group(1)).decode(), headers={"referer": domain})
+            link = base64.b64decode(media.group(1)).decode()
+            links[link] = net.hlsurl(link, headers={"referer": domain})
         else:
             for script in htmlement.fromstring(src).iterfind(".//script"):
-                if script.get("src") and "yayin" in script.get("src"):
+                ssrc = script.get("src")
+                if ssrc and ("yayin" in ssrc or "/play.php" in ssrc):
                     scriptsrc = net.http(script.get("src"), referer=domain)
                     key = re.search(rgxkey, scriptsrc)
                     if key:
                         for link in re.findall(rgxlink, scriptsrc):
                             if "anahtar" in link:
                                 link = net.absurl(link, script.get("src"))
-                                yield net.hlsurl(link + key.group(1), headers={"referer": domain})
-                                break
+                                links[link] = net.hlsurl(link + key.group(1), headers={"referer": domain})
+        for link in links.values():
+            yield link
