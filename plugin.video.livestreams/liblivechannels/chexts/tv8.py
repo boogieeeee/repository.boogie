4a91986d08d1829f2d4e2f8d88a2d7312771ab3f
boogiepop <boogiepop@gmx.com>
2022-01-30 15:31:15 +0100

    livestreams: tv8 more sources

--- plugin.video.livestreams/liblivechannels/chexts/tv8.py
+++ plugin.video.livestreams/liblivechannels/chexts/tv8.py
@@ -21,6 +21,7 @@ class tv8(multi, scraper):
     selcuk_name = "8"
     ses_id = "tv8-izle"
     kolay_ids = ["/tv8-canli-hd", "/tv8-canli-hd/2", "/tv8-canli-hd/3"]
+    canlitv_ids = ["tv8-canli-hd", "tv8-canli-hd/2"]
 
     def get(self):
         for stream in multi.get(self):
