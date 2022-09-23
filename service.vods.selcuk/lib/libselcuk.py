8d94a260edfb866421c412e4798868af3da1f56a
boogiepop <boogiepop@gmx.com>
2022-09-23 22:35:00 +0200

    selcuk: new vods service

---
+++ service.vods.selcuk/lib/libselcuk.py
@@ -0,0 +1,56 @@
+'''
+Created on Sep 23, 2022
+
+@author: boogie
+'''
+import htmlement
+import re
+import base64
+
+from tinyxbmc import tools
+from tinyxbmc import net
+from tinyxbmc.addon import kodisetting
+
+from six.moves.urllib import parse
+
+maxlink = 5
+subpath = None
+rgx1 = "\s*[a-z]+\s?\:\s?(?:\'|\")(.+?)(?:\'|\")"
+rgx2 = "window\.streamradardomil\s*?\=\s*?(.+?)\;"
+rgx3 = "window\.streamradardomil\s*?\=\s*?(.+?)\;"
+rgx4 = "atob\((?:\"|\')([a-zA-Z0-9\=]+?)(?:\"|\')\)"
+rgx5 = "window._[a-f0-9]+\s*?\=\s*?window\[.atob.\]\((?:\'|\")([A-Za-z0-9\=]+)(?:\'|\")"
+rgx6 = "src\s*?\=\s*?(?:\"|\')(\/keslanorospu.+?)(?:\"|\')"
+setting = kodisetting("service.vods.selcuk")
+
+
+def iteratechannels():
+    entrypage = net.http("https://%s/" % setting.getstr("domain"), cache=10)
+    url = htmlement.fromstring(entrypage).findall(".//div[@class='sites']/.//a")[0].get("href")
+    xpage = htmlement.fromstring(net.http(url, cache=10))
+    links = xpage.findall(".//div[@class='channels']/div[2]/.//a")
+    for link in links:
+        chname = tools.elementsrc(link.find(".//div[@class='name']"), exclude=[link.find(".//b")]).strip()
+        chlink = link.get("data-url")
+        if chlink.startswith("http"):
+            yield url, chlink, chname
+
+
+def getmedias(url, selcukurl, isadaptive=False):
+    links = url.split("#")
+    up = parse.urlparse(links[0])
+    chdict = dict(parse.parse_qsl(up.query))
+    if "id" in chdict:
+        subpage = net.http(links[0], referer=selcukurl, cache=None)
+        olmusmu = re.findall(rgx1, subpage)
+        if len(olmusmu) == 3:
+            keslan = re.search(rgx6, subpage)
+            kourl = "%s://%s/%s" % (up.scheme, up.netloc, keslan.group(1))
+            kopage = net.http(kourl, referer=selcukurl, cache=None)
+            bases = [base64.b64decode(x).decode() for x in re.findall(rgx4, kopage)]
+            _radardom = bases.pop(-1)
+            selcukdom = bases.pop(-1)
+            for base in bases:
+                if "." not in base:
+                    media = "https://" + base + selcukdom + "/i/" + olmusmu[-1] + "/" + chdict["id"] + "/playlist.m3u8" + olmusmu[-2]
+                    yield net.hlsurl(media, headers={"referer": url}, adaptive=isadaptive)
---
+++ service.vods.selcuk/lib/selcuk.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+    Author    : Huseyin BIYIK <husenbiyik at hotmail>
+    Year      : 2016
+    License   : GPL
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import vods
+from tinyxbmc.const import DB_TOKEN
+import libselcuk
+
+
+class selcuk(vods.movieextension):
+    dropboxtoken = DB_TOKEN
+    uselinkplayers = False
+    useaddonplayers = False
+
+    info = {"title": "Selcuk Sports"
+            }
+
+    def getmovies(self):
+        for selcukurl, url, chname in libselcuk.iteratechannels():
+            self.additem(chname, [url, selcukurl])
+
+    def geturls(self, url):
+        for media in libselcuk.getmedias(*url):
+            yield media
