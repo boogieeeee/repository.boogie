5575a839818c93c9fd1e33a683ef4679080178d3
boogie <boogiepop@gmx.com>
2023-02-24 15:42:39 +0100

    aceengine: rewrite the client api

--- plugin.program.aceengine/aceengine/__init__.py
+++ plugin.program.aceengine/aceengine/__init__.py
@@ -1,40 +1,83 @@
+import time
+import json
+
 from . import const
 from tinyxbmc import addon
 from tinyxbmc import net
+from tinyxbmc import hay
+import aceengine
 
 
 settings = addon.kodisetting(const.ADDONID)
 
+class AcestreamError(Exception):
+    pass
 
-def isrunning():
-    return settings.getbool(const.SETTING_ISRUNNING)
 
+class acestream():
+    def __init__(self, url):
+        self.url = url
+        self.id = url.lower().replace("acestream://", "").strip()
+        self.stat_url = None
+        self.command_url = None
+        self.playback_url = None
+        self.stats = {}
+        self.lastupdate = 0
 
-def apiurl():
-    if isrunning():
+    def getstream(self):
+        if not self.playback_url:
+            jsdata = self.query("%s/ace/getstream?id=%s&format=json" % (acestream.apiurl(), self.id), ignore=True)
+            if jsdata:
+                self.stat_url = jsdata["stat_url"]
+                self.command_url = jsdata["command_url"]
+                self.playback_url = jsdata["playback_url"]
+                self.updatestats()
+    
+    @property
+    def httpurl(self):
+        self.stopall()
+        self.getstream()
+        return self.playback_url
+
+    def stopall(self, ignoreme=True):
+        with hay.stack(const.STREAMHAY, aid=const.ADDONID) as streamhay:
+            streams = streamhay.find(const.STREAMHAY_KEY).data
+            for sid in list(streams.keys()):
+                if ignoreme and sid == self.id:
+                    continue
+                self.stop(streams[sid][2])
+                streams.pop(sid)
+            streams[self.id] = (self.stat_url, self.command_url, self.playback_url)
+            streamhay.throw(const.STREAMHAY_KEY, streams)
+
+    def query(self, uri, ignore=False):
+        jsdata = net.http(uri, cache=None)
+        try:
+            jsdata = json.loads(jsdata)
+        except Exception:
+            jsdata = {"error": "JsonError: %s" % jsdata,
+                      "response": None}
+        if jsdata.get("error") and not ignore:
+            raise AcestreamError(jsdata["error"])
+        return jsdata.get("response")
+        
+    def updatestats(self):
+        if (time.time() - self.lastupdate) > 1:
+            self.stats = self.query(self.stat_url)
+            self.lastupdate = time.time()
+        
+    @property
+    def hasstarted(self):
+        self.updatestats()
+        return self.stats["status"] == "dl"
+        
+    def stop(self, url=None):
+        if not url:
+            url = self.command_url
+        if url:
+            retval = self.query(url + "?method=stop", ignore=True) == "ok"
+            return retval is None or retval == "ok"
+    
+    @staticmethod
+    def apiurl():
         return settings.getstr(const.SETTING_ACTIVEADDRESS)
-
-
-def aceurl(url):
-    api = apiurl()
-    if api:
-        return "%s/ace/getstream?id=%s" % (api, url.lower().replace("acestream://", "").strip())
-
-
-def _get_engine_url(acestreamurl, key):
-    result = net.http(acestreamurl.kodiurl + "&format=json", cache=None, json=True)
-    if "response" in result:
-        return result["response"].get(key)
-
-
-def stats(acestreamurl):
-    statu = _get_engine_url(acestreamurl, "stat_url")
-    if statu:
-        return net.http(statu, cache=None, json=True).get("response", {})
-    return {}
-
-
-def stop(acestreamurl):
-    cmdu = _get_engine_url(acestreamurl, "command_url")
-    if cmdu:
-        return net.http(cmdu + "?method=stop", cache=None, json=True).get("response")
