9e1c774508786071c953a08c520aa280f0d078be
boogie <boogiepop@gmx.com>
2023-12-09 22:40:58 +0100

    livestreams: canlitv: fix test link

--- plugin.video.livestreams/liblivechannels/chexts/scrapertools/canlitvcenter.py
+++ plugin.video.livestreams/liblivechannels/chexts/scrapertools/canlitvcenter.py
@@ -9,7 +9,7 @@ try:
 
     class testcanli(unittest.TestCase):
         def test_canli_link(self):
-            test.testlink(self, itermedias("tv-8-canli-hd/2"), 1, "tv8", 0)
+            test.testlink(self, itermedias("tv8-izle-hd-canli"), 1, "tv8", 0)
 
 except ImportError:
     pass
