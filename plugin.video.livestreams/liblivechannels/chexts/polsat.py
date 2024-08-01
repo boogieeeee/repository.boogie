f40434e58831c7886cdd7b283cde143b8a8d8532
boogie <boogiepop@gmx.com>
2024-08-01 20:15:51 +0200

    polsat: fix names and EPGs

--- plugin.video.livestreams/liblivechannels/chexts/polsat.py
+++ plugin.video.livestreams/liblivechannels/chexts/polsat.py
@@ -40,13 +40,13 @@ epgu = "https://www.polsatsport.pl/ajax-program-tv-column/module/"
 
 class polsatsport(multi, scraper):
     icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Polsat_Sport_2021_gradient.svg/200px-Polsat_Sport_2021_gradient.svg.png"
-    title = "Polsat Sport"
+    title = "Polsat Sport 1"
     categories = ["Polish", "Sport"]
     dady_name = "polsatsportpoland"
     acestreams = ["acestream://92b6ba09dae4bbb8a67a405125d08ca8d15380ee"]
 
     def iterprogrammes(self):
-        for p in iterprogrammes("POLSAT Sport"):
+        for p in iterprogrammes(self.title):
             yield p
 
 
@@ -58,7 +58,7 @@ class polsatpremium1(multi, scraper):
     acestreams = ["acestream://4ce55db7e578ff52a6e526cf610e477464b8a99f"]
 
     def iterprogrammes(self):
-        for p in iterprogrammes("POLSAT Sport Premium 1"):
+        for p in iterprogrammes(self.title):
             yield p
 
 
@@ -70,19 +70,19 @@ class polsatpremium2(multi, scraper):
     acestreams = ["acestream://3147c586346aee00c005d1caa55204d38678c95f"]
 
     def iterprogrammes(self):
-        for p in iterprogrammes("POLSAT Sport Premium 2"):
+        for p in iterprogrammes(self.title):
             yield p
 
 
 class polsatsportextra(multi, scraper):
     icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Polsat_Sport_Extra_2021_gradient.svg/240px-Polsat_Sport_Extra_2021_gradient.svg.png"
-    title = "Polsat Sport Extra"
+    title = "Polsat Sport 2"
     categories = ["Polish", "Sport"]
     dady_name = "polsatsportextrapoland"
     acestreams = ["acestream://ef2cf11fc83f4f15a33c9a514a7afd2bce73122f"]
     
     def iterprogrammes(self):
-        for p in iterprogrammes("POLSAT Sport Extra"):
+        for p in iterprogrammes(self.title):
             yield p
 
 
@@ -94,7 +94,7 @@ class polsatsportnews(multi, scraper):
     dady_name = "polsatsportnewspoland"
 
     def iterprogrammes(self):
-        for p in iterprogrammes("POLSAT Sport News"):
+        for p in iterprogrammes(self.title):
             yield p
 
 
@@ -103,7 +103,7 @@ def iterprogrammes(polsatname):
     for pagenum in [1, 2]:
         page = json.loads(net.http(epgu + "page%s" % pagenum, cache=None, encoding="utf-8"))
         for channel in page["channels"]:
-            if channel["title"] == polsatname:
+            if channel["title"].lower() == polsatname.lower():
                 for prog in channel["programs"]:
                     cur_start = datetime.fromtimestamp(prog["emissionDate"] / 1000)
                     cur_start.replace(tzinfo=tools.tz_utc())
