a1a30bc58554910667287ebe9223379ba1f2df2f
boogie <boogiepop@gmx.com>
2025-06-17 18:20:15 +0200

    daddy: fix scrapers

--- service.vods.poscitech/lib/liblivetvon.py
+++ service.vods.poscitech/lib/liblivetvon.py
@@ -15,8 +15,7 @@ from six.moves.urllib import parse
 
 setting = kodisetting("service.vods.poscitech")
 domain = "https://" + setting.getstr("domain")
-mrgx = "source\s*?\:\s*?(?:\'|\")(.+?)(?:\'|\")"
-dtrgx = "([0-9]{1,2})[\sa-zA-Z]*?(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)[a-zA-Z\s]*?([0-9]{4})"
+dtrgx = r"([0-9]{1,2})[\sa-zA-Z]*?(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)[a-zA-Z\s]*?([0-9]{4})"
 
 monthtoint = {"jan": 1,
               "feb": 2,
@@ -33,19 +32,37 @@ monthtoint = {"jan": 1,
 
 
 def geturl(streamid):
-    u = "%s/embed/stream-%s.php" % (domain, streamid)
+    u = "%s/stream/stream-%s.php" % (domain, streamid)
     xiframe = htmlement.fromstring(net.http(u, referer=domain))
     iframeu = xiframe.find(".//iframe[@id='thatframe']").get("src")
     iframe = net.http(iframeu, referer=u)
-    src = re.findall(mrgx, iframe)
-    ref = parse.urlparse(iframeu)
-    host = re.search(",'([a-zA-Z0-9\.\-]+?)'\);", iframe).group(1)
-    orig = "%s://%s" % (ref.scheme, ref.netloc)
-    ref = "%s://%s/" % (ref.scheme, ref.netloc)
-    headers={"Host": host,
+    vars = {"channelKey": "",
+            "authTs": "",
+            "authRnd": "",
+            "authSig": "",}
+    for k, v in vars.items():
+        vars[k] = re.search(f"var\s+{k}\s+=\s+(?:\"|\')(.+?)(?:\"|\');", iframe).group(1)
+    authurl = re.search(r"(?:\"|\')(.+?auth\.php.*?)\?", iframe).group(1)
+    params = {"channel_id": vars["channelKey"],
+              "ts": vars["authTs"],
+              "rnd": vars["authRnd"],
+              "sig": vars["authSig"]}
+    auth = net.http(net.absurl(authurl, iframeu), params=params, json=True)
+    lookupurl = authurl = re.search(r"(?:\"|\')(.+?server_lookup\.php.*?)\?", iframe).group(1)
+    params = {"channel_id": vars["channelKey"]}
+    lookup = net.http(net.absurl(lookupurl, iframeu), params=params, json=True)
+    server_key = lookup["server_key"]
+    pre = server_key.split("/")[0]
+    post = server_key
+    murl = f"https://{pre}new.newkso.ru/{post}/{vars['channelKey']}/mono.m3u8"
+    
+    iframeu_p = parse.urlparse(iframeu)
+    orig = iframeu_p.scheme + "://" + iframeu_p.netloc
+    headers={
              "Origin": orig,
-             "Referer": ref}
-    return mediaurl.hlsurl(src[-1], headers=headers, adaptive=True, ffmpegdirect=False,
+             "Referer": orig + "/",
+             }
+    return mediaurl.hlsurl(murl, headers=headers, adaptive=True, ffmpegdirect=False,
                            lurl="", lheaders=headers, lbody="", lresponse="", license=None)
 
 
@@ -64,15 +81,16 @@ def getschdate(page):
 def getevents():
     allevents = []
     loctz = tools.tz_local()
-    schedules = ["schedule-extra-generated.json", "schedule-generated.json", "extra2-schedule.php"]
+    schedules = ["schedule-extra-generated.php", "schedule-generated.php", "extra2-schedule-2.php"]
     for schedule in schedules:
-        js = net.http(f"{domain}/schedule/{schedule}", json=True)
+        js = net.http(f"{domain}/schedule/{schedule}", referer=domain, json=True)
         for dt, members in js.items():
             sch_date = getschdate(dt)
             for category, events in members.items():
+                category = re.sub(r"<.*?>", "", category).strip()
                 for event in events:
                     title = event["event"]
-                    evdtmatch = re.search("([0-9]{1,2})\:([0-9]{1,2})(.+)", event["time"])
+                    evdtmatch = re.search(r"([0-9]{1,2})\:([0-9]{1,2})(.*)", event["time"])
                     hour = int(evdtmatch.group(1))
                     minute = int(evdtmatch.group(2))
                     evdate = datetime(hour=hour, minute=minute,
@@ -83,6 +101,7 @@ def getevents():
                         if(channel["channel_id"]).isdigit():
                             channels.append([channel["channel_name"], int(channel["channel_id"])])
                     allevents.append([evdate.astimezone(loctz), category, title, channels])
+    allevents.sort()
     return allevents
 
 
@@ -92,7 +111,7 @@ def getchmeta(numbyname=False, nameidbynum=False):
     for a in htmlement.fromstring(page).iterfind(".//div[@class='grid-item']/a"):
         href = a.get("href")
         if href is not None:
-            chnum = re.search("stream\-([0-9]+)\.", href)
+            chnum = re.search(r"stream\-([0-9]+)\.", href)
             if chnum:
                 chnum = int(chnum.group(1))
                 if numbyname:
