d77e483b454879e055acd4dab443d63b374f35db
boogie <boogiepop@gmx.com>
2025-06-18 18:26:03 +0200

    smu: fix patching

--- plugin.program.boogie-players/lib/smu.py
+++ plugin.program.boogie-players/lib/smu.py
@@ -45,63 +45,72 @@ def patchsmu(smudir):
         else:
             return ""
 
+    smudir = os.path.join(smudir, "script.module.resolveurl")
     with hay.stack("smupatch") as smuhay:
         smupatch = smuhay.find("smupatch").data
-        with open(os.path.join(smudir, "..", "ResolveURL.json")) as vf:
+        with open(os.path.join(smudir, "..", "..", "ResolveURL.json")) as vf:
             versioncommit = json.load(vf)
         versioncommit = versioncommit.get("latest")
         pldir = addon.get_addon("plugin.program.boogie-players").getAddonInfo("path")
-        if not smupatch.get("versioncommit") == versioncommit:
-            # first remove xbmcaddon referenced to script.module.urlresolver
-            files = [(["script.module.resolveurl", "lib", "resolveurl", "lib", "log_utils.py"], 1),
-                     (["script.module.resolveurl", "lib", "resolveurl", "lib", "kodi.py"], 1),
-                     (["script.module.resolveurl", "lib", "resolveurl", "common.py"], 2),
-                     (["script.module.resolveurl", "lib", "resolveurl", "lib", "CustomProgressDialog.py"], 1),
-                     ]
-            for fpaths, patchtype in files:
-                fpath = os.path.join(smudir, *fpaths)
-                print(fpath)
-                with open(fpath, "r") as f:
-                    contents = f.read()
-                if patchtype == 1:
-                    pattern = "script\.module\.resolveurl"
-                    sub = "plugin.program.boogie-players"
-                elif patchtype == 2:
-                    pattern = "settings_file \= os\.path\.join\(addon_path, 'resources', 'settings.xml'\)"
-                    sub = "settings_file = os.path.join(os.path.dirname(__file__), '..', '..', 'settings.xml')"
-                if re.search(pattern, contents):
-                    with open(fpath, "w") as f:
-                        f.write(re.sub(pattern, sub, contents))
 
-            # synchronize resources folder with xbmcvfs in case of permission problem
-            for root, folders, files in os.walk(os.path.join(smudir, "resources")):
-                relfolder = os.path.relpath(root, smudir)
-                for folder in folders:
-                    tools.mkdirs(os.path.join(pldir, relfolder, folder))
-                for f in files:
-                    sfile = os.path.join(smudir, relfolder, f)
-                    tfile = os.path.join(pldir, relfolder, f)
-                    with tools.File(sfile) as sfileo:
-                        with tools.File(tfile, "w") as tfileo:
-                            tfileo.write(sfileo.readBytes())
+        if smupatch.get("versioncommit_v2") == versioncommit:
+            return
 
-            from resolveurl import _update_settings_xml
-            from resolveurl import common
-            _update_settings_xml()
-            smuxmlp = common.settings_file
-            plxmlop = os.path.join(pldir, "resources", "settings_orig.xml")
-            plxmlp = os.path.join(pldir, "resources", "settings.xml")
-            with tools.File(smuxmlp, "r") as smuxml:
-                with tools.File(plxmlop, "r") as plxmlo:
-                    with tools.File(plxmlp, "w") as plxml:
-                        try:
-                            plxml.write('<?xml version="1.0" ?><settings>%s%s</settings>' %
-                                        (getstr(plxmlo.read()), getstr(smuxml.read())))
-                        except Exception:
-                            print(traceback.format_exc())
-                            shutil.copyfile(plxmlop, plxmlp)
+        # first remove xbmcaddon referenced to script.module.urlresolver
+        files = [(["lib", "resolveurl", "lib", "log_utils.py"], 1),
+                 (["lib", "resolveurl", "lib", "kodi.py"], 1),
+                 (["lib", "resolveurl", "lib", "kodi.py"], 3),
+                 (["lib", "resolveurl", "common.py"], 2),
+                 (["lib", "resolveurl", "lib", "CustomProgressDialog.py"], 1),
+                 ]
+        for fpaths, patchtype in files:
+            fpath = os.path.join(smudir, *fpaths)
+            with open(fpath, "r") as f:
+                contents = f.read()
+            if patchtype == 1:
+                pattern = "script\.module\.resolveurl"
+                sub = "plugin.program.boogie-players"
+            elif patchtype == 2:
+                pattern = "settings_file \= os\.path\.join\(addon_path, 'resources', 'settings.xml'\)"
+                sub = "settings_file = os.path.join(os.path.dirname(__file__), '..', '..', 'settings.xml')"
+            elif patchtype == 3:
+                pattern = "xbmcaddon.Addon\(\)"
+                sub = "xbmcaddon.Addon('plugin.program.boogie-players')"
+            if re.search(pattern, contents):
+                with open(fpath, "w") as f:
+                    f.write(re.sub(pattern, sub, contents))
 
-            smupatch["versioncommit"] = versioncommit
+        # synchronize resources folder with xbmcvfs in case of permission problem
+        for root, folders, files in os.walk(os.path.join(smudir, "resources")):
+            relfolder = os.path.relpath(root, smudir)
+            for folder in folders:
+                tools.mkdirs(os.path.join(pldir, relfolder, folder))
+            for f in files:
+                sfile = os.path.join(smudir, relfolder, f)
+                tfile = os.path.join(pldir, relfolder, f)
+                with tools.File(sfile) as sfileo:
+                    with tools.File(tfile, "w") as tfileo:
+                        tfileo.write(sfileo.readBytes())
+
+        from resolveurl import _update_settings_xml
+        from resolveurl import common
+        _update_settings_xml()
+        smuxmlp = common.settings_file
+        plxmlop = os.path.join(pldir, "resources", "settings_orig.xml")
+        plxmlp = os.path.join(pldir, "resources", "settings.xml")
+        with tools.File(smuxmlp, "r") as smuxml:
+            with tools.File(plxmlop, "r") as plxmlo:
+                with tools.File(plxmlp, "w") as plxml:
+                    try:
+                        plxml.write('<?xml version="1.0" ?><settings>%s%s</settings>' %
+                                    (getstr(plxmlo.read()), getstr(smuxml.read())))
+                    except Exception:
+                        print(traceback.format_exc())
+                        shutil.copyfile(plxmlop, plxmlp)
+
+            
+
+            smupatch["versioncommit_v2"] = versioncommit
             smuhay.throw("smupatch", smupatch)
             tools.builtin("UpdateLocalAddons()")
             # gui.ok("URL Resolvers", "SMU has just been updated\nSome changes will be active on the next run")
