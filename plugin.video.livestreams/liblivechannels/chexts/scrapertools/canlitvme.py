bca356a01ea85efa3a4b76efcfed618103b67b50
boogie <boogiepop@gmx.com>
2025-07-11 23:18:46 +0200

    canlitv: add mirror site

--- plugin.video.livestreams/liblivechannels/chexts/scrapertools/canlitvme.py
+++ plugin.video.livestreams/liblivechannels/chexts/scrapertools/canlitvme.py
@@ -15,12 +15,17 @@ from tinyxbmc import mediaurl
 import htmlement
 import re
 
-domain = "https://www.canlitv.me"
+domains = {"https://www.canlitv.vin": [".//ul[@class='kanallar']/.//a",
+                                       ".//div[@class='alternatif']/ul/.//a"],
+           "https://www.canlitv.me": [".//li[@class='canlitvlist']/.//a",
+                                      ".//ul[@class='alternatif']/.//a"],
+           }
+
 
 def deobfuslink(page):
-    fileaddrs = re.findall('changeVideo\((?:\'|\")(.+?)(?:\'|\")\)', page)
+    fileaddrs = re.findall(r'changeVideo\((?:\'|\")(.+?)(?:\'|\")\)', page)
     if not fileaddrs:
-        fileaddrs = re.findall('file\s*?\:\s*?(?:\'|\")(.+?)(?:\'|\")', page)
+        fileaddrs = re.findall(r'file\s*?\:\s*?(?:\'|\")(.+?)(?:\'|\")', page)
     for fileaddr in fileaddrs:
         shift = int(re.search("(^[0-9]+)", fileaddr).group(1))
         prev_c = None
@@ -35,7 +40,7 @@ def deobfuslink(page):
             prev_c = c
         fileaddr = fileaddr[start_index:]
         arrays = []
-        for arr in re.findall("\=\s*?(\[.+?\])", page):
+        for arr in re.findall(r"\=\s*?(\[.+?\])", page):
             try:
                 isvalid = True
                 arr = eval(arr)
@@ -46,31 +51,30 @@ def deobfuslink(page):
                 if not isvalid:
                     continue
                 arrays.append(arr)
-            except:
+            except Exception:
                 continue
         translation = dict(zip(arrays[0][shift:] + arrays[0][:shift], arrays[3]))
         yield fileaddr.translate(fileaddr.maketrans(translation))
 
-def searchkw(kw):
-    spage = htmlement.fromstring(net.http(domain, referer=domain))
-    form = spage.find(".//form")
-    sec = form.find(".//input[@name='security']").get("value")
-    lpage = htmlement.fromstring(net.http(form.get("action"), params={"s": kw, "security": sec}, referer=domain))
-    for channel in lpage.iterfind(".//li[@class='canlitvlist']/.//a"):
-        chname = channel.get("title").lower().replace(" ", "")
-        if chname == kw.lower().replace(" ", ""):
-            chpage = htmlement.fromstring(net.http(channel.get("href"), referer=domain))
-            for alt in chpage.iterfind(".//ul[@class='alternatif']/.//a"):
-                yield alt.get("href")
 
-
-def itermedias(keyword):
-    for u in searchkw(keyword):
-        upage = htmlement.fromstring(net.http(u, referer=domain))
-        geo = upage.find(".//iframe").get("src")
-        gpage = net.http(geo, referer=u)
-        yayin = re.findall('src="(.+?ulke.+?)"', gpage)[-1]
-        yayin = yayin.replace("'+ulke+'", "DE")
-        ypage = net.http(yayin, referer=geo)
-        for ylink in tools.safeiter(deobfuslink(ypage)):
-            yield mediaurl.hlsurl(ylink, headers={"referer": domain + "/"}, adaptive=True, ffmpegdirect=False)
+def itermedias(kw):
+    for domain, [xpath1, xpath2] in domains.items():
+        spage = htmlement.fromstring(net.http(domain, referer=domain))
+        form = spage.find(".//form")
+        sec = form.find(".//input[@name='security']").get("value")
+        lpage = htmlement.fromstring(net.http(form.get("action"), params={"s": kw, "security": sec}, referer=domain))
+        for channel in lpage.iterfind(xpath1):
+            chname = channel.get("title").lower().replace(" ", "")
+            if chname == kw.lower().replace(" ", ""):
+                chpage = htmlement.fromstring(net.http(channel.get("href"), referer=domain))
+                for alt in chpage.iterfind(xpath2):
+                    u = alt.get("href")
+                    upage = htmlement.fromstring(net.http(u, referer=domain))
+                    geo = upage.find(".//iframe").get("src")
+                    gpage = net.http(geo, referer=u)
+                    yayin = re.findall('src="(.+?ulke.+?)"', gpage)[-1]
+                    yayin = yayin.replace("'+ulke+'", "DE")
+                    ypage = net.http(yayin, referer=geo)
+                    for ylink in tools.safeiter(deobfuslink(ypage)):
+                        yield mediaurl.hlsurl(ylink, headers={"referer": domain + "/",
+                                                              "origin": domain}, adaptive=True, ffmpegdirect=False)
