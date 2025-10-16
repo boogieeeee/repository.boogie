29de7101078685def6ae98cd6ce790eaf12b1f89
boogie <boogiepop@gmx.com>
2025-10-16 14:20:09 +0200

    taraf: robust domain handling

--- service.vods.taraftarium/lib/libtaraftarium.py
+++ service.vods.taraftarium/lib/libtaraftarium.py
@@ -14,9 +14,28 @@ from tinyxbmc.addon import kodisetting
 from six.moves.urllib import parse
 
 SETTING = kodisetting("service.vods.taraftarium")
-DOMAIN = SETTING.getstr("domain")
-MAINURL = f"https://{DOMAIN}"
 RGX1 = r"baseurl\s*?\=\s*?(?:\"|\')(.+?)(?:\"|\')"
+DOMAINS = ["noingoaithatgiare.com",
+           "taraftariumizle.in",
+           "pressedfur.com",
+           "taraftarium-amp.xyz",
+           "movicta.com",
+           "rvlgames.com",
+           "lovelytera.com"]
+
+
+def getmainpage():
+    setting_domain = SETTING.getstr("domain")
+    for domain in [setting_domain] + DOMAINS:
+        url = f"https://{domain}/index.html"
+        try:
+            resp = net.http(url, text=False)
+        except Exception:
+            continue
+        if resp.status_code < 200 or resp.status_code > 300:
+            continue
+        SETTING.set("domain", domain)
+        return url, resp.content
 
 
 def makekeyw(txt):
@@ -27,16 +46,19 @@ def makekeyw(txt):
 
 
 def iteratechannels():
-    page = net.http(MAINURL)
+    page = getmainpage()[1]
     xpage = htmlement.fromstring(page)
     for div in xpage.iterfind(".//div[@class='macListe']/div"):
-        chname = tools.elementsrc(div.find(".//span[@class='takimlar']")).strip()
+        chdiv = div.find(".//span[@class='takimlar']")
+        if chdiv is None:
+            continue
+        chname = tools.elementsrc(chdiv).strip()
         link = div.get("data-url")
         yield link, chname
 
 
 def geturls(link):
-    subpage = net.http(link, referer=MAINURL)
+    subpage = net.http(link, referer=getmainpage()[0])
     baseurl = re.search(RGX1, subpage).group(1)
     up = parse.urlparse(link)
     origin = f"{up.scheme}://{up.netloc}"
